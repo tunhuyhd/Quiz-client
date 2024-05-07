1ca8c080fc1dbd1b21ffb4d7c05a3f3d46deb660
tunghuy22t2 <nguyentunghuy02@gmail.com>
2024-05-07 21:18:29 +0700

    Init and Login page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CssBaseline, createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
